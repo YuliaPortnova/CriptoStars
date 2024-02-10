b97db3fe5670a2dee18b09e782dee8a3777b1eb6
YuliaPortnova <yuyul@yandex.ru>
2024-02-10 22:46:53 +0300

    feat: блокирует страницу для перемещения табом при открытом модальном окне

--- js/transaction/modal.js
+++ js/transaction/modal.js
@@ -1,5 +1,6 @@
 const modal = document.querySelector('.modal');
 const closeButton = modal.querySelector('.modal__close-btn');
+const firstTabButton = modal.querySelector('.btn--exchange-all');
 
 const onDocumentKeydown = (event) => {
   if (event.key.startsWith('Esc')) {
@@ -13,12 +14,21 @@ const onModalClick = (event) => {
   }
 };
 
+let onDocumentFocus;
+
 const openModal = () => {
   modal.style.display = 'flex';
   modal.scroll(0, 0);
   document.body.classList.add('scroll-lock');
   document.addEventListener('keydown', onDocumentKeydown);
   modal.addEventListener('click', onModalClick);
+  onDocumentFocus = (event) => {
+    if (!modal.contains( event.target ) ) {
+      event.stopPropagation();
+      firstTabButton.focus();
+    }
+  };
+  document.addEventListener('focus', onDocumentFocus, true);
 };
 
 const closeModal = () => {
@@ -26,6 +36,7 @@ const closeModal = () => {
   document.body.classList.remove('scroll-lock');
   document.removeEventListener('keydown', onDocumentKeydown);
   modal.removeEventListener('click', onModalClick);
+  document.removeEventListener('focus', onDocumentFocus, true);
 };
 
 closeButton.addEventListener('click', () => closeModal());
